d45d0e00e6f1a7e5fc47c9c5ed86cbdef1dacdfc
Romulo Soares <romulodcf@gmail.com>
2025-04-22 23:24:14 -0300

    Aplicando identação

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 import Image from "next/image";
 
 function Home() {
-    return <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        flexDirection: 'column'
-    }}>
-        <Image
-            src="/img_madruginha.png"
-            alt="Madruguinha"
-            width={380}
-            height={380}
-        ></Image>
-        <h1>"As pessoas boas devem amar seus inimigos"</h1>
-    </div>
+  return <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    flexDirection: 'column'
+  }}>
+    <Image
+      src="/img_madruginha.png"
+      alt="Madruguinha"
+      width={380}
+      height={380}
+    ></Image>
+    <h1>"As pessoas boas devem amar seus inimigos"</h1>
+  </div>
 
 }
 
