df046f9db01e6a2efab934fed81de8f421f67c7f
Romulo Soares <romulodcf@gmail.com>
2025-05-14 00:03:32 -0300

    add database.js

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,5 @@
 test("GET to /api/v1/status sould return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch("http://localhost:3001/api/v1/status");
+  const data = await response.json();
   expect(response.status).toBe(200);
 });
